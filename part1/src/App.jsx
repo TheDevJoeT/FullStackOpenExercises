2d301cda9f87b1c17a5500b9ae71e7a9dbfa034e
JoeT1327 <thantziaung.joe631@gmail.com>
2026-02-17 10:02:54 +0630

    components and event handlers section complete - part1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,46 +1,27 @@
-import React from 'react'
+import { useState } from "react";
 
-// Header component
-const Header = ({ courseName }) => <h1>{courseName}</h1>
+const Display = ({ counter }) => <div>{counter}</div>;
 
-// Part component
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-// Content component
-const Content = ({ parts }) => {
-  return (
-    <div>
-      <Part part={parts[0]} />
-      <Part part={parts[1]} />
-      <Part part={parts[2]} />
-    </div>
-  )
-}
-
-// Total component
-const Total = ({ parts }) => {
-  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
-  return <p>Number of exercises {total}</p>
-}
-
-// App component
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      { name: 'Fundamentals of React', exercises: 10 },
-      { name: 'Using props to pass data', exercises: 7 },
-      { name: 'State of a component', exercises: 14 }
-    ]
-  }
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+
+  const decreaseByOne = () => setCounter(counter - 1);
+
+  const setToZero = () => setCounter(0);
 
   return (
     <div>
-      <Header courseName={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
+      <Display counter={counter} />
 
-export default App
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </div>
+  );
+};
+
+export default App;
