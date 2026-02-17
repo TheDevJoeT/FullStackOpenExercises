0ea3acb930b9d337e24c19f2b5ebaa46a5bea55b
JoeT1327 <thantziaung.joe631@gmail.com>
2026-02-17 13:03:54 +0630

    asynchoronously

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,25 +1,37 @@
 import { useState } from "react";
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
-  const handleLeftClick = () =>
-  setClicks({ ...clicks, left: clicks.left + 1 }) 
-  // ...clicks is used to copy the existing state, and then we update the left property with the new value.
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
+  };
 
-const handleRightClick = () =>
-  setClicks({ ...clicks, right: clicks.right + 1 })
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  };
 
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
-      {clicks.right}
+      {right}
+
+      <p>{allClicks.join(" ")}</p>
+
+      <p>Total clicks: {total}</p>
     </div>
-  )
-}
+  );
+};
 
 export default App;
