afc357019d6f842e46b433c21f21f0e17a1885d7
JoeT1327 <thantziaung.joe631@gmail.com>
2026-02-17 14:46:45 +0630

    exercise 1.8

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,9 +1,27 @@
 import { useState } from "react";
 
-// 1. Reusable Button component
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
+
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+  const all = good + neutral + bad;
+  const average = all === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / all;
+  const positive = all === 0 ? 0 : (good / all) * 100;
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  );
+};
 
 const App = () => {
   const [good, setGood] = useState(0);
@@ -14,25 +32,15 @@ const App = () => {
   const handleNeutral = () => setNeutral(neutral + 1);
   const handleBad = () => setBad(bad + 1);
 
-  const all = good + neutral + bad;
-  const average = all === 0 ? 0 : (good * 1 + neutral * 0 + bad * -1) / all;
-  const positive = all === 0 ? 0 : (good / all) * 100;
-
   return (
     <div>
       <h1>give feedback</h1>
-     
+
       <Button handleClick={handleGood} text="good" />
       <Button handleClick={handleNeutral} text="neutral" />
       <Button handleClick={handleBad} text="bad" />
 
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
