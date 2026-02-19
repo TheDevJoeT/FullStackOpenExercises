e073484a52a347501ee6afd96ab776cd75c4ddc9
JoeT1327 <thantziaung.joe631@gmail.com>
2026-02-19 20:38:28 +0630

    exercise 2.19

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -4,65 +4,77 @@ import axios from "axios";
 const App = () => {
   const [search, setSearch] = useState("");
   const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
   useEffect(() => {
-  axios
-    .get("https://restcountries.com/v3.1/all?fields=name,capital,area,flags,languages,cca3")
-    .then(response => {
-      setCountries(response.data)
-    })
-}, [])
-
+    axios
+      .get(
+        "https://restcountries.com/v3.1/all?fields=name,capital,area,flags,languages,cca3",
+      )
+      .then((response) => {
+        setCountries(response.data);
+      });
+  }, []);
 
   const filteredCountries = countries.filter((country) =>
     country.name.common.toLowerCase().includes(search.toLowerCase()),
   );
 
-  console.log("Countries loaded:", countries.length)
+  const countryToShow =
+    selectedCountry ||
+    (filteredCountries.length === 1 ? filteredCountries[0] : null);
+
+  console.log("Countries loaded:", countries.length);
 
   return (
     <div>
       <div>
         find countries{" "}
-        <input value={search} onChange={(e) => setSearch(e.target.value)} />
+        <input
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+            setSelectedCountry(null);
+          }}
+        />
       </div>
 
       {search && (
         <>
-          {filteredCountries.length > 10 && (
+          {!countryToShow && filteredCountries.length > 10 && (
             <div>Too many matches, specify another filter</div>
           )}
 
-          {filteredCountries.length <= 10 && filteredCountries.length > 1 && (
+          {!countryToShow &&
+            filteredCountries.length <= 10 &&
+            filteredCountries.length > 1 && (
+              <div>
+                {filteredCountries.map((country) => (
+                  <div key={country.cca3}>
+                    {country.name.common}
+                    <button onClick={() => setSelectedCountry(country)}>
+                      show
+                    </button>
+                  </div>
+                ))}
+              </div>
+            )}
+
+          {countryToShow && (
             <div>
-              {filteredCountries.map((country) => (
-                <div key={country.cca3}>{country.name.common}</div>
-              ))}
-            </div>
-          )}
+              <h2>{countryToShow.name.common}</h2>
 
-          {filteredCountries.length === 1 && (
-            <div>
-              <h2>{filteredCountries[0].name.common}</h2>
-
-              <div>Capital {filteredCountries[0].capital?.[0]}</div>
-
-              <div>Area {filteredCountries[0].area}</div>
+              <div>Capital {countryToShow.capital?.[0]}</div>
+              <div>Area {countryToShow.area}</div>
 
               <h3>Languages</h3>
               <ul>
-                {Object.values(filteredCountries[0].languages || {}).map(
-                  (lang) => (
-                    <li key={lang}>{lang}</li>
-                  ),
-                )}
+                {Object.values(countryToShow.languages || {}).map((lang) => (
+                  <li key={lang}>{lang}</li>
+                ))}
               </ul>
 
-              <img
-                src={filteredCountries[0].flags.png}
-                alt="flag"
-                width="200"
-              />
+              <img src={countryToShow.flags.png} alt="flag" width="200" />
             </div>
           )}
         </>
