896685c79a4e51c3e1c1c4e62ff36649d818db15
JoeT1327 <thantziaung.joe631@gmail.com>
2026-02-23 19:43:06 +0630

    exercise 3.6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -66,6 +66,15 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body;
+
+    if(!body.name || !body.number) {
+        return response.status(400).json({ error: 'name or number is missing' });
+    }
+
+    if(persons.find(person => person.name === body.name)) {
+        return response.status(400).json({ error: 'name must be unique' });
+    }
+
     const newPerson = {
         id: generateId(),
         name: body.name,
