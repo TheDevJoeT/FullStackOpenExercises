8175ef5335d9532ec0db1701c835b7ebd0088cd7
JoeT1327 <thantziaung.joe631@gmail.com>
2026-02-26 11:25:08 +0630

    exercise 4.4

--- part4/bloglist-backend/tests/list_helper.test.js
+++ part4/bloglist-backend/tests/list_helper.test.js
@@ -8,3 +8,56 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+const { describe } = require('node:test')
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '1',
+      title: 'First blog',
+      author: 'Joe',
+      url: 'http://example.com',
+      likes: 5,
+    }
+  ]
+
+  const listWithManyBlogs = [
+    {
+      _id: '1',
+      title: 'First blog',
+      author: 'Joe',
+      url: 'http://example.com',
+      likes: 5,
+    },
+    {
+      _id: '2',
+      title: 'Second blog',
+      author: 'Jane',
+      url: 'http://example.com',
+      likes: 7,
+    },
+    {
+      _id: '3',
+      title: 'Third blog',
+      author: 'Joe',
+      url: 'http://example.com',
+      likes: 3,
+    }
+  ]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('when list has many blogs, returns total likes', () => {
+    const result = listHelper.totalLikes(listWithManyBlogs)
+    assert.strictEqual(result, 15)
+  })
+
+  test('when list is empty, returns zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+})
