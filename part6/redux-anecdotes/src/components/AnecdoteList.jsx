2daa88fe236f533947a5a56be369d52f970d4850
JoeT1327 <thantziaung.joe631@gmail.com>
2026-03-10 10:26:48 +0630

    exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,20 +1,24 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { useDispatch, useSelector } from "react-redux";
+import { voteAnecdote } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const anecdotes = useSelector(state =>
-    [...state].sort((a, b) => b.votes - a.votes)
-  )
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    return anecdotes
+      .filter((anecdote) =>
+        anecdote.content.toLowerCase().includes(filter.toLowerCase()),
+      )
+      .sort((a, b) => b.votes - a.votes);
+  });
 
   const vote = (id) => {
-    dispatch(voteAnecdote(id))
-  }
+    dispatch(voteAnecdote(id));
+  };
 
   return (
     <div>
-      {anecdotes.map(anecdote => (
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
@@ -24,7 +28,7 @@ const AnecdoteList = () => {
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteList
+export default AnecdoteList;
